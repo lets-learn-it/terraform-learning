e8306d987a1ecaa169ea862f1d25de8547ed87e5
Parikshit Patil <patil8698parikshit@gmail.com>
2021-09-05 21:28:43 +0530

    small changes

--- 01-create-ec2-instance-with-s3-as-backend/ec2.tf
+++ 01-create-ec2-instance-with-s3-as-backend/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "my_ec2" {
-  ami             = var.ami_id
-  instance_type   = "t2.micro"
+  ami           = var.ami_id
+  instance_type = "t2.micro"
 
   tags = var.tags
 }
--- 01-create-ec2-instance-with-s3-as-backend/provider.tf
+++ 01-create-ec2-instance-with-s3-as-backend/provider.tf
@@ -1,4 +1,4 @@
 provider "aws" {
-  region = "ap-south-1"
+  region  = "ap-south-1"
   version = "3.37"
 }
