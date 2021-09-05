5e0c98fcacc2443a794c4ab4556c49492ee2802d
Parikshit Patil <patil8698parikshit@gmail.com>
2021-09-05 21:30:45 +0530

    attach role to ec2

--- 02-ec2-access-s3/ec2.tf
+++ 02-ec2-access-s3/ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "my_ec2" {
   instance_type        = "t2.micro"
   key_name             = aws_key_pair.key.key_name
   security_groups      = [aws_security_group.allow_ssh.name]
-  # iam_instance_profile = aws_iam_instance_profile.test_profile.name
+  iam_instance_profile = aws_iam_instance_profile.test_profile.name
 
   tags = var.tags
 }
