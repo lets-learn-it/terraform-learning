14d7a13f5263b1ffb88a522a88ee328677267151
Parikshit Patil <patil8698parikshit@gmail.com>
2021-10-24 00:09:34 +0530

    changes in vm instance

--- 00-vm-instance/ip.tf
+++ 00-vm-instance/ip.tf
@@ -1,5 +1,5 @@
 resource "azurerm_public_ip" "public_ip" {
-  name                = "acceptanceTestPublicIp1"
+  name                = "vm_public_ip"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
   allocation_method   = "Dynamic"
--- 00-vm-instance/sg.tf
+++ 00-vm-instance/sg.tf
@@ -1,11 +1,11 @@
-resource "azurerm_network_security_group" "example" {
-  name                = "acceptanceTestSecurityGroup1"
+resource "azurerm_network_security_group" "nsg" {
+  name                = "ssh_nsg"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 
   security_rule {
-    name                       = "test123"
-    priority                   = 100
+    name                       = "allow_ssh_sg"
+    priority                   = 100 
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
@@ -14,4 +14,9 @@ resource "azurerm_network_security_group" "example" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+}
+
+resource "azurerm_network_interface_security_group_association" "association" {
+  network_interface_id      = azurerm_network_interface.example.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
 }
